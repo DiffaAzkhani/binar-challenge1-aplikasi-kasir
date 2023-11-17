d712aa77c6faff6f137e921786d8574984cafa99
Diffa Azkhani <diffaazkhani1@gmail.com>
2023-11-17 08:27:21 +0700

    Progress Challenge 6 : Belum

---
+++ src/main/java/binar/bej1/diffaazkhani/BinarFudChallenge4/config/Config.java
@@ -0,0 +1,26 @@
+package binar.bej1.diffaazkhani.BinarFudChallenge4.config;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.logging.Logger;
+
+//@Slf4j
+//@Configuration
+//public class Config {
+
+//    @Value("${nama.kelas.binar}")
+//    private String namaKelas; // ""
+//
+//    Config(RoleRepository roleRepository) {
+//    }
+
+//    @Bean
+//    public String basicConfiguration() {
+//        log.info("Basic configuration is initializing. . .");
+//        log.info("Initialize success by {}", namaKelas);
+//        return this.namaKelas;
+//    }
+//}
